3362f7e9a53c279a532fea3cb040d6acdc08bb04
KalukiKilukumi <kalukikilukumi@gmail.com>
2023-03-17 03:05:51 -0700

    prints with proper grammar, but the outcome is a piece of artusing the printf function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main -entry point for program
+ *
+ * Return: 0 if no error, non zero value if error
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
