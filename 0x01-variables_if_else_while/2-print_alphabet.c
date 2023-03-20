344ebf4d8067e4505b33912451f57b538ce19df3
KalukiKilukumi <kalukikilukumi@gmail.com>
2023-03-20 13:12:40 -0700

    prints alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,18 @@
-vivif
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Description: prints alphabet in lowercase followed by new line
+ * Return: 0
+ */
+int main(void)
+{
+	int lt;
+
+	for (lt = 'a'; lt <= 'z'; lt++)
+	{
+		putchar(lt);
+	}
+	putchar('\n');
+	return (0);
+}
