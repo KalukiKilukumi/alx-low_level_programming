43431217af75269b309431d0577b1f439814fc3f
KalukiKilukumi <kalukikilukumi@gmail.com>
2023-03-22 05:22:36 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
  * main - entry point
- * Description: prints _putchar followed bt new line
+ * Description: prints _putchar followed by new line
  * Return: 0
  */
 
 int main(void)
 {
-_putchar("-");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
