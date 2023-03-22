a7c28810100b4abf7429fecc0d9d3ac6936f9faa
KalukiKilukumi <kalukikilukumi@gmail.com>
2023-03-22 05:33:27 -0700

    prints lowecase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - entry point 
+ * main -entry point
  * print_alphabet: prints alphabet in lowercase
  * Return: 0
  */
- 
+
 void print_alphabet(void);
 {
 	char letter;
@@ -12,7 +12,6 @@ void print_alphabet(void);
 	for (letter = 'a'; letter <= 'z'; letter++)
 		-putchar(letter);
 	_putchar('\n');
-	return (0);
 }
 
 
