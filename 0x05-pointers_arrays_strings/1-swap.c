fbd579aaa1ec5f3ce8b9ea9350199481f586083f
KalukiKilukumi <kalukikilukumi@gmail.com>
2023-03-27 12:17:27 -0700

    swaps values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -10,6 +10,6 @@ void swap_int (int *a, int *b)
 	 int m;/* used as temporary storage*/
 
 	 m = *a; /*a value is put into temporary storage*/
-	 *a = *b; /* b value is put into temporary storage*/
-	 *b = *m; /* a value in temporary storage is put into b and vice versa*/
+	 *a = *b; /*b value is put into temporary storage*/
+	 *b = *m; /*a value in temporary storage is put into b and vice versa*/
 }
