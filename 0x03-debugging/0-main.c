b976219e9fbef2277274eec10709d958af993596
KalukiKilukumi <kalukikilukumi@gmail.com>
2023-03-29 03:00:23 -0700

    tells us whether number is positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - determine whether a number is negative, positive or zero
+ * main - determine whether a number is negative or positive
  * i: number to be checked
  * Return:0 on success
  */
@@ -11,7 +11,6 @@ int main(void)
 int i;
 
 i = 0;
-
 positive_or_negative(i);
 
 return (0);
