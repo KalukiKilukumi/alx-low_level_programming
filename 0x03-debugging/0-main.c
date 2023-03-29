260a8bdb1fc84d136ab8b51d68a8d5420fbe363a
KalukiKilukumi <kalukikilukumi@gmail.com>
2023-03-29 02:58:20 -0700

    tells us whether number is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - determine whether a number is negative, positive or zero
+ * i: number to be checked
+ * Return:0 on success
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+
+positive_or_negative(i);
+
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,4 @@
+DEBUGGING
+
+Debugging is the process of finding and fixing errors in software that prevents it from running correctly.
+These tasks are part of the ALX program in C Debugging
