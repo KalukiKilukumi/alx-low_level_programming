7c2b2a7fe586307f33bd1e5ef5e66f6b1924f41f
KalukiKilukumi <kalukikilukumi@gmail.com>
2023-04-04 12:45:14 -0700

    computes the absolute value of an integer.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,11 +4,12 @@
  * @n: the int to check
  * Return: the absolute value of int
  */
-int_abs(int n)
+int _abs(int n)
 {
 	if (n >= 0)
 	{
 		return (n);
 	}
-	return (-n);
+	else
+		return (-n);
 }
