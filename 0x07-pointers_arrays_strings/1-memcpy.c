4e19d572d45c398f17a65242a5c32ae9964003c3
KalukiKilukumi <kalukikilukumi@gmail.com>
2023-04-04 02:18:08 -0700

    fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies memory area
+ * @src: memory source
+ * @dest: memory destination
+ * @n: number of bytes
+ *
+ * Return: copied memory with nbytes changes
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int j = 0;
+	int i = n;
+
+	for (;j < n; j++)
+	{
+		dest[j] = src[j];
+		n--;
+	}
+	return (dest);
+}
